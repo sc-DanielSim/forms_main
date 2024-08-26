8231267e8d9a20fac699cc239141e55a8f30211f
sc-DanielSim <dsd@sitecore.net>
2024-08-26 16:04:42 +0800

    GetImageFieldSerializerExtendCustom

---
+++ src/platform/Custom/GetImageFieldSerializerExtendCustom.cs
@@ -0,0 +1,21 @@
+﻿using Sitecore.Diagnostics;
+using Sitecore.LayoutService.Serialization;
+using Sitecore.LayoutService.Serialization.Pipelines.GetFieldSerializer;
+
+namespace XmCloudSXAStarter.Custom
+{
+	public class GetImageFieldSerializerExtendCustom : BaseGetFieldSerializer
+	{
+		public GetImageFieldSerializerExtendCustom(IFieldRenderer fieldRenderer)
+			: base(fieldRenderer)
+		{
+		}
+
+		protected override void SetResult(GetFieldSerializerPipelineArgs args)
+		{
+			Assert.ArgumentNotNull(args, "args");
+			args.Result = new ImageFieldSerializerExtendCustom(FieldRenderer);
+		}
+	}
+
+}
---
+++ src/platform/Custom/ImageFieldSerializerExtendCustom.cs
@@ -0,0 +1,59 @@
+﻿using HtmlAgilityPack;
+using Sitecore.LayoutService.Serialization;
+using Sitecore.LayoutService.Serialization.FieldSerializers;
+using System.Collections.Generic;
+using System.Web;
+
+namespace XmCloudSXAStarter.Custom
+{
+    public class ImageFieldSerializerExtendCustom : ImageFieldSerializer
+    {
+        public ImageFieldSerializerExtendCustom(IFieldRenderer fieldRenderer)
+          : base(fieldRenderer)
+        {
+        }
+
+        protected override IDictionary<string, string> ParseRenderedImage(
+          string renderedField)
+        {
+            Dictionary<string, string> renderedImage = new Dictionary<string, string>();
+            HtmlDocument htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(renderedField);
+            if (htmlDocument.DocumentNode == null || !htmlDocument.DocumentNode.HasChildNodes)
+                return (IDictionary<string, string>)renderedImage;
+            HtmlNode htmlNode1 = htmlDocument.DocumentNode.SelectSingleNode("//img");
+            if (htmlNode1 == null)
+            {
+                htmlNode1 = htmlDocument.DocumentNode.SelectSingleNode("//image");
+                if (htmlNode1 == null)
+                {
+                    htmlNode1 = htmlDocument.DocumentNode.SelectSingleNode("//video");
+                    if (htmlNode1 == null)
+                    {
+                        htmlNode1 = htmlDocument.DocumentNode.SelectSingleNode("//a");
+                        if (htmlNode1 == null)
+                            return (IDictionary<string, string>)renderedImage;
+                        renderedImage["dam-content-type"] = "Document";
+                        renderedImage["label"] = htmlNode1.InnerText;
+                    }
+                    else
+                    {
+                        HtmlNode htmlNode2 = htmlDocument.DocumentNode.SelectSingleNode("//source");
+                        renderedImage["dam-content-type"] = "Video";
+                        foreach (HtmlAttribute attribute in (IEnumerable<HtmlAttribute>)htmlNode2.Attributes)
+                            renderedImage[attribute.Name] = HttpUtility.HtmlDecode(attribute.Value);
+                    }
+                }
+            }
+            foreach (HtmlAttribute attribute in (IEnumerable<HtmlAttribute>)htmlNode1.Attributes)
+                renderedImage[attribute.Name] = HttpUtility.HtmlDecode(attribute.Value);
+            
+            if(renderedImage.ContainsKey("dam-id") && renderedImage["dam-id"].Length > 0)
+            {
+                renderedImage["stylelabs-content-id"] = renderedImage["dam-id"];
+                renderedImage["stylelabs-content-type"] = renderedImage["dam-content-type"];
+            }
+
+            return (IDictionary<string, string>)renderedImage;
+        }
+}
